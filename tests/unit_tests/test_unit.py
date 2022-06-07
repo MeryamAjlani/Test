9176a4083cef46118296e128bf843f6deff9e257
Maryem El Ajleni <ajlanimeryam@gmail.com>
2022-06-07 22:02:12 +0200

    fix final

--- tests/unit_tests/test_unit.py
+++ tests/unit_tests/test_unit.py
@@ -4,65 +4,30 @@ from unittest.mock import patch
 
 
 class TestEmployee(TestCase):
-    @patch("app.sqlite3")
-    def test_get_employee_existant(self, mock_class):
-        # given
-        mock_class.connect().execute().fetchone.return_value = (4, 'paul','paul@gmail.com','88888888')
-
-        expected_employee = {
-            "id": 4,
-            "name": "paul",
-            "email":"paul@gmail.com",
-            "phone":"88888888"
-        }
-
-        # when
-        item = fetch_by_id(4)
-
-        # then
-        assert item == expected_employee
-    
-    @patch("app.sqlite3")
-    def test_get_employee_inexistant(self, mock_class):
-        # given
-        mock_class.connect().execute().fetchone.return_value = (7, 'paul','paul@gmail.com','88888888')
-
-        expected_employee = {
-            "id": 4,
-            "name": "paul",
-            "email":"paul@gmail.com",
-            "phone":"88888888"
-        }
-
-        # when
-        item = fetch_by_id(7)
-
-        # then
-        assert item == expected_employee
     
     @patch("app.sqlite3")
     def test_get_all(self, mock_class):
             # Given
-            mock_class.connect().cursor().execute().fetchall.return_value = [(2,'mathiddd','mathis@gmail.com','88888888'),
-            (4,'paul','paul@gmail.com','88888888'),
-            (6,'wassime','wassimeCenter@gmail.com','12345678'),
-            (7,'maryem','ajlanimeryam@gmail.com','12345678')],
+            mock_class.connect.return_value.cursor.return_value.execute.return_value.fetchall.return_value = [(1,'mahdi','mahdi@gmail.com','88888888'),(2,'mathis','mathis@gmail.com','88888888'),
+            (3,'joran','joran@gmail.com','88888888'),
+             (5,'paul','paul@gmail.com','88888888'),
+            (6,'Sarah','sarah@gmail.com','12345678')],
 
-            expected_product = [(2,'mathiddd','mathis@gmail.com','88888888'),
-            (4,'paul','paul@gmail.com','88888888'),
-            (6,'wassime','wassimeCenter@gmail.com','12345678'),
-            (7,'maryem','ajlanimeryam@gmail.com','12345678')]
+            expected_product =  [(1,'mahdi','mahdi@gmail.com','88888888'),(2,'mathis','mathis@gmail.com','88888888'),
+            (3,'joran','joran@gmail.com','88888888'),
+             (5,'paul','paul@gmail.com','88888888'),
+            (6,'Sarah','sarah@gmail.com','12345678')],
              # When
             result_product =fetch_all()
             # Then
             self.assertEqual(expected_product, result_product)
     
     @patch("app.sqlite3")
-    def test_addProduct(self, mocked_object):
+    def test_addEmployee(self, mocked_object):
         # Given
         mock_execute=(mocked_object.connect.return_value.execute)
         # When
-        add_employee('test', 0,0)
+        add_employee('insat','insat@gmail.com','111111')
         # Then
         mock_execute.assert_called_once()
 
@@ -78,7 +43,7 @@ class TestEmployee(TestCase):
         mock_execute.assert_called_once()
 
     @patch("app.sqlite3")
-    def test_deleteProduct(self, mocked_object):
+    def test_deleteEmployee(self, mocked_object):
         # Given
         mock_execute=(mocked_object.connect.return_value.execute)
         # When
